7c9210a91bdcd18403a72c6cd124072bfebc05aa
jean-jaures <oka.jeanjaures@sncf.fr>
2024-06-24 14:27:20 +0200

    new announcement page

---
+++ src/app/ad/new/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+import React, {useState} from "react";
+import Form from "@/components/ad-multi-step-form";
+
+export default function NewAdPage () {
+    return (<div><Form/></div>)
+}
