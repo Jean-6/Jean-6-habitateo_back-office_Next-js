bf0b6a89d5588149886b613d2a61cc7ce2830ab3
jean-jaures <oka.jeanjaures@sncf.fr>
2024-07-11 15:25:04 +0200

    registration form

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -8,6 +8,9 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {z} from "zod";
 
+import PhoneInput from "react-phone-input-2";
+import 'react-phone-number-input/style.css'
+
 type Inputs = z.infer<typeof SchemaRegistration>
 
 const RegistrationForm = () => {
@@ -28,111 +31,128 @@ const RegistrationForm = () => {
     const {
         register,
         handleSubmit,
-        watch,
+        //watch,
         reset,
-        trigger,
+        //trigger,
         formState: { errors }
     } = useForm<Inputs>({
         resolver: zodResolver(SchemaRegistration)
     })
+
     const registrationForm: SubmitHandler<Inputs> = data => {
-        console.log(data)
-        reset()
+    console.log(data)
+    reset()
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(registrationForm)}  className="space-y-3" >
-                <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                    <div className="max-w-md w-full space-y-8">
-                        <div>
-                            <img
-                                className="mx-auto h-12 w-auto"
-                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                alt="Workflow"
-                            />
-                            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Authentification</h2>
-                            {<p className="mt-2 text-center text-sm text-gray-600">
-                                Ou{' '}
-                                <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                    Se connecter
-                                </a>
-                            </p>}
-                        </div>
-                        {/**/}
-                        <div>
-                            <label htmlFor="lastname">Nom</label>
+        <main>
+        <div  className="space-y-3">
+            <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-md w-full space-y-8">
+                    <div>
+                        <img
+                            className="mx-auto h-12 w-auto"
+                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                            alt="Workflow"
+                        />
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Creer un compte</h2>
+                        {<p className="mt-2 text-center text-sm text-gray-600">
+                            Ou{' '}
+                            <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                Se connecter
+                            </a>
+                        </p>}
+                    </div>
+                    {/**/}
+                    <form onSubmit={handleSubmit(registrationForm)} className="mt-8 space-y-6" > {/*onSubmit={handleSubmit(registrationForm)}*/}
+                        <input type="hidden" name="remember" defaultValue="true" />
+                        <div className="rounded-md shadow-sm -space-y-px">
+
+                            <div className="mb-1">
+                                <label htmlFor="lastname">Nom</label>
                                 <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="lastname" type="text" placeholder="Nom" {...register('lastname')}  />
-                            {errors.lastname?.message && (
-                                <p className='mt-2 text-sm text-red-400'>
-                                    {errors.lastname.message}
-                                </p>
-                            )}
-                        </div>
-                        <div>
-                            <label htmlFor="firstname">Prenom(s)</label>
+                                {errors.lastname?.message && (
+                                    <p className='mt-2 text-sm text-red-400'>
+                                        {errors.lastname.message}
+                                    </p>
+                                )}
+                            </div>
+
+                            <div className="">
+                                <label htmlFor="firstname">Prenom(s)</label>
                                 <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="firstname" type="text" placeholder="Prenom(s)" {...register('firstname')}/>
-                            {errors.firstname?.message && (
-                                <p className='mt-2 text-sm text-red-400'>
-                                    {errors.firstname.message}
-                                </p>
-                            )}
-                        </div>
-                        <div>
-                            <label htmlFor="email">Email</label>
-                                <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" {...register('email')} />
-                            {errors.email?.message && (
-                                <p className='mt-2 text-sm text-red-400'>
-                                    {errors.email.message}
-                                </p>
-                            )}
-                        </div>
-                        <div>
-                            <label htmlFor="password">Mot de passe</label>
+                                {errors.firstname?.message && (
+                                    <p className='mt-2 text-sm text-red-400'>
+                                        {errors.firstname.message}
+                                    </p>
+                                )}
+                            </div>
+
+                            <div className="">
+                                <label htmlFor="email">Email</label>
+                                <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" {...register('email')}/>
+                                {errors.email?.message && (
+                                    <p className='mt-2 text-sm text-red-400'>
+                                        {errors.email.message}
+                                    </p>
+                                )}
+
+                            </div>
+                            <div className="">
+                                <label htmlFor="password">Mot de passe</label>
                                 <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" {...register('password')}/>
-                            {errors.password?.message && (
-                                <p className='mt-2 text-sm text-red-400'>
-                                    {errors.password.message}
-                                </p>
-                            )}
-                        </div>
-                        {/*<div>
-                            <label htmlFor="phone">Tel.</label>
+                                {errors.password?.message && (
+                                    <p className='mt-2 text-sm text-red-400'>
+                                        {errors.password.message}
+                                    </p>
+                                )}
+                            </div>
+
+                            <div className="">
+                                <label htmlFor="phone">Telephone</label>
+                                <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="phone" type="text" placeholder="N°" {...register('phoneNumber')}/>
+                                {errors.phoneNumber?.message && (
+                                    <p className='mt-2 text-sm text-red-400'>
+                                        {errors.phoneNumber.message}
+                                    </p>
+                                )}
+                            </div>
+
+                            {/*<div className="">
+                                <label htmlFor="phone">Tel.</label>
                                 <PhoneInput
                                     country={'fr'}
                                     value={phoneNumber}
                                     onChange={handleChangePhoneNumber}
                                     placeholder="Telephone"
                                 />
-                            {errors.phone?.message && (
-                                <p className='mt-2 text-sm text-red-400'>
-                                    {errors.phone.message}
-                                </p>
-                            )}
-                        </div>*/}
-                        <div className="flex items-center justify-between">
-                            <div className="text-sm">
-                                <button className="font-medium text-indigo-600 hover:text-indigo-500" onClick={() => setShowModal(true)}>  Verification Email</button>
-                            </div>
-
-                            <div className="text-sm">
-                                <a href="/password" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                    Mot de passe oublié
-                                </a>
-                            </div>
+                            </div>*/}
                         </div>
+
+
+
                         <div>
                             <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2focus:ring-indigo-500"><span className="absolute left-0 inset-y-0 flex items-center pl-3"> </span>Inscription</button>
                         </div>
+                    </form>
+                    <div className="flex items-center justify-between">
+
+                        <div className="text-sm">
+                            <button className="font-medium text-indigo-600 hover:text-indigo-500" onClick={() => setShowModal(true)}>  Verification Email</button>
+                        </div>
+
+                        <div className="text-sm">
+                            <a href="/password" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                Mot de passe oublié
+                            </a>
+                        </div>
                     </div>
+                    <VerifyUserEmailModal isVisible={showModal} onClose={() => setShowModal(false)}/>
 
                 </div>
-
-            </form>
-            <VerifyUserEmailModal isVisible={showModal} onClose={() => setShowModal(false)}/>
+            </div>
         </div>
-    );
-
+    </main>)
 }
 
 export default RegistrationForm;
